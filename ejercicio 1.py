c1c78d716b0955370215ae26143085e4c5913eb6
jennyillescas <tatillescas@gmail.com>
2023-03-30 19:04:11 -0500

    ejercicio 1

---
+++ ejercicio 1.py
@@ -0,0 +1,13 @@
+print("Fundamentos de Python")
+print("Nombre: Jennifer Illescas")
+print("Fecha: 30 de marzo del 2023 \n")
+numero1=int(input("Ingrese un numero: "))
+numero2=int(input("Ingrese un numero: "))
+valor1=numero1==numero2
+valor2=numero1!=numero2
+valor3=numero1>numero2
+valor4=numero1<=numero2
+print("Los valores son iguales ",valor1)
+print("Los valores son diferentes ",valor2)
+print("El primero numero es mayor al segundo ", valor3)
+print("El segundo valor es mayor o igual que el primero ",valor4)
