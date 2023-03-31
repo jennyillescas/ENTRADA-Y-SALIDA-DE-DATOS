d839c98359e6bed4316413c3f516155277811b3d
jennyillescas <tatillescas@gmail.com>
2023-03-30 19:25:36 -0500

    ejercicio 3

---
+++ ejercicio 3.py
@@ -0,0 +1,12 @@
+print("Fundamentos de Python")
+print("Nombre: Jennifer Illescas")
+print("Fecha: 30 de marzo del 2023 \n")
+nombre=input("Ingrese su nombre: ")
+#OPCION 1 
+print("¡Hola "+str(nombre)+"!")
+#OPCION 2
+print("¡Hola",nombre,'!')
+#OPCION 3
+print(f"¡Hola {nombre}!")
+#OPCION 4
+print("¡Hola {}!".format(nombre))
